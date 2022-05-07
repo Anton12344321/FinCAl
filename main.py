daa0393170f0525b74d66a642f52bf82a33ee7be
Anton12344321 <105113895+Anton12344321@users.noreply.github.com>
2022-05-07 18:41:30 +0500

    Update main.py
    version 0.2(test code)

--- main.py
+++ main.py
@@ -12,25 +12,27 @@ class MainWindow(QWidget):
         self.connects()
         self.set_appear()
         self.show()
-        def initUI(self):
-            self.btn_send = QPushButton(txt_send, self)
-            self.line = QLineEdit(txt_line)
-            self.lable_finish = QLabel()
-            self.layout_line = QHBoxLayout()
-            self.layout_line.addWidget(self.line, alignment = Qt.AlignLeft)
-            self.layout_line.addWidget(self.btn_send, alignment = Qt.AlignLeft)
-            self.layout_line.addWidget(self.lable_finish, alignment = Qt.AlignCenter)
-            self.setLayout(self.layout_line)
-        def next_click(self):
-            self.lable_finish.setText(self.line.text())
-        def connects(self):
-            self.btn_send.clicked.connect(self.next_click)
-        def set_appear(self):
-            self.setWindowTitle(txt_title)
-            self.resize(win_width, win_height)
-            self.move(win_x, win_y)
-    def main():
-        app = QApplication([])
-        mw = MainWindow()
-        app.exec_()
+    def initUI(self):
+        self.btn_send = QPushButton(txt_send, self)
+        self.line = QLineEdit(txt_line)
+        self.lable_finish = QLabel()
+        self.layout_line = QHBoxLayout()
+        self.layout_line.addWidget(self.line, alignment = Qt.AlignLeft)
+        self.layout_line.addWidget(self.btn_send, alignment = Qt.AlignLeft)
+        self.layout_line.addWidget(self.lable_finish, alignment = Qt.AlignCenter)
+        self.setLayout(self.layout_line)
+    def next_click(self):
+        self.lable_finish.setText(self.line.text())
+    def connects(self):
+        self.btn_send.clicked.connect(self.next_click)
+    def set_appear(self):
+        self.setWindowTitle(txt_title)
+        self.resize(win_width, win_height)
+        self.move(win_x, win_y)
+def main():
+    app = QApplication([])
+    window = MainWindow()
+    window.show()
+    app.exec_()
+main()
 
